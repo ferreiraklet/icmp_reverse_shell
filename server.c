a58e0999c58a3b9e08848e3d3a240bfefe203e76
ferreiraklet <90654646+ferreiraklet@users.noreply.github.com>
2022-11-06 16:51:48 -0300

    Memset in command, reset buffer

--- server.c
+++ server.c
@@ -1,3 +1,35 @@
+/*
+
+ *   icmpsh - simple icmp command shell
+
+ *   Copyright (c) 2010, Nico Leidecker <nico@leidecker.info>
+
+ *   This program is free software: you can redistribute it and/or modify
+
+ *   it under the terms of the GNU General Public License as published by
+
+ *   the Free Software Foundation, either version 3 of the License, or
+
+ *   (at your option) any later version.
+
+ *
+
+ *    This program is distributed in the hope that it will be useful,
+
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *    GNU General Public License for more details.
+
+ *
+
+ *    You should have received a copy of the GNU General Public License
+
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 
 
 #include <stdio.h>
@@ -390,9 +422,13 @@ void *cmd_exec(){
 
         //icmp_sendrequest(sockfd, inet_addr("192.168.1.9"), command, sizeof(command));
 
+            //printf("COMMANDO: %s", command);
+
             icmp_sendrequest(sockfd, inet_addr("192.168.1.9"), command, sizeof(command));
 
             icmp_sendrequest(sockfd, inet_addr("192.168.1.9"), command, sizeof(command));
+
+            memset(command, 0x00, 2048);
 
             //printf("cmd $ ");
 
